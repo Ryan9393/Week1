89c3a23cb09b882b58f6555a500346c537c3530d
Ryan9393 <mryan9393@gmail.com>
2022-05-31 20:21:37 -0400

    created js file

---
+++ index.js
@@ -0,0 +1,9 @@
+var name = "double equals checks just the value triple equals checks the type aswell";
+var item1 = 3;
+var item2 = 3;
+
+var total = item1 == item2;
+
+console.log (total);
+
+console.log(name);
